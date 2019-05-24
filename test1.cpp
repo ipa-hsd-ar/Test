53f22ee56e00efb6b73c2c0b655e36844d0b96e6
Abirami Ravi <abirami.ravi@ipa.fraunhofer.de>
2019-05-24 14:21:51 +0200

    Message on the work policy

--- test1.cpp
+++ test1.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout<<"Hello world";
     cout<<"I am going to rock the stage";
     cout<<"I am an artist, I am going to create great things.";
     cout<<"For achieving anything you need patience";
+    
+    cout<<"Feature 1: Always be chill";
+    cout<<"Less tension more work";
     
     return 0;
 }
